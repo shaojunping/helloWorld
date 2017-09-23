9cf135e04dd9141fb9f4e17e59616cbba8ee34b7
shaojunping <853561376@qq.com>
2017-09-23 20:08:21 +0800

    加高光

--- light/BasicLighting.cpp
+++ light/BasicLighting.cpp
@@ -181,6 +181,7 @@ int main()
 		ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 		ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		ourShader.setVec3("lightPos", lightPos);
+		ourShader.setVec3("viewPos", camera.Position);
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetCameraMatrix();
--- light/shader/2.1.basic_lighting.fs
+++ light/shader/2.1.basic_lighting.fs
@@ -7,17 +7,28 @@ in vec3 FragPos;
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 viewPos;
 
 void main()
 {
-	float ambientStrength = 0.5;
+    //ambient
+	float ambientStrength = 0.1;
 	vec3 ambient = ambientStrength * lightColor;
 
+	//diffuse
+	//float diffuseStrength = 0.5;
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0);
 	vec3 diffuse = diff * lightColor;
 
-	vec3 result = (ambient + diffuse) * objectColor;
+	//specular
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPos - lightPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+	vec3 specular = spec * lightColor * specularStrength;
+
+	vec3 result = (ambient + diffuse + specular) * objectColor;
 	FragColor = vec4(result, 1.0);
 }
