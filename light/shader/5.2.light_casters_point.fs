fdbbdba3fbd9ffcb46748dbbb6decfd2183461f2
shaojunping <853561376@qq.com>
2017-10-08 19:16:44 +0800

    点光源

--- light/shader/5.2.light_casters_point.fs
+++ light/shader/5.2.light_casters_point.fs
@@ -14,15 +14,12 @@ struct Material
 
 struct Light
 {
-	vec3 position;
-	
+	//vec3 position;
+	vec3 direction;
+
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
-	
-	float constant;
-	float linear;
-	float quadratic;
 };
 
 uniform Material material;
